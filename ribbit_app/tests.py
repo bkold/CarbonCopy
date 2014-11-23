36a45896486cac5dafa2d64f6530d2778630d98e
srujank1 <ksrujan@msn.com>
2014-11-22 21:32:58 -0700

    Update tests.py
    I did some selenium functional testing.

--- ribbit_app/tests.py
+++ ribbit_app/tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
@@ -12,6 +5,16 @@ from django.conf.urls import url
 from ribbit_app.models import Ribbit
 from . import views
 from django.core.urlresolvers import reverse 
+
+from django.test import LiveServerTestCase
+from django.contrib.auth.models import User
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import unittest, time, re
+from time import time
+
 
 class TestAccess(TestCase):
 	def setUp(self):
@@ -123,3 +126,56 @@ class TestLogedInAccess(TestCase):
 		Ribbit.objects.create(content='test post 2', pic='{{MEDIA_URL}}uploaded_files/test.jpg', brightness='20', user=self.user) 		
 		response = self.c.get(reverse('sub'))
 		
+class TestWebdriver(LiveServerTestCase):
+    def setUp(self):
+		self.driver = webdriver.Firefox()
+		User.objects.create_superuser(
+			username='admin',
+			password='admin',
+			email='admin@example.com'
+        )
+        
+
+		
+    def tearDown(self):
+		# Call tearDown to close the web browser
+		self.driver.quit()
+
+    def test_auth_user(self): 
+		self.driver.get('http://127.0.0.1:8000/')
+
+		self.driver.implicitly_wait(10)
+		username = self.driver.find_element_by_xpath('//input[@placeholder="Username"]')
+		username.send_keys("test_new")		# This needs to change evertime 
+		password1 = self.driver.find_element_by_id("id_email")
+		password1.send_keys("testuser@test.com")
+		password1 = self.driver.find_element_by_id("id_password1")
+		password1.send_keys("123")
+		password2 = self.driver.find_element_by_id("id_password2")
+		password2.send_keys("123")
+		self.driver.find_element_by_xpath('//input[@value="Create Account"]').click()
+		self.driver.implicitly_wait(10)
+		self.driver.find_element_by_link_text("Public Profiles").click()
+		self.driver.implicitly_wait(10)
+		self.driver.find_element_by_link_text("My Profile").click()
+		self.driver.implicitly_wait(10)
+		self.driver.find_element_by_link_text("Public Posts").click()
+		self.driver.find_element_by_xpath('//input[@value="Log Out"]').click()
+
+    def test_login_user(self):
+		self.driver.get('http://127.0.0.1:8000/')
+
+		username =  self.driver.find_element_by_id("id_username")
+		username.send_keys("root")       #this needs to be a vaild user
+		password =  self.driver.find_element_by_id("id_password")
+		password.send_keys("123")
+		self.driver.implicitly_wait(10)
+		self.driver.find_element_by_xpath('//input[@value="Log In"]').click()
+		self.driver.implicitly_wait(10)
+		self.driver.find_element_by_link_text("Home").click()
+		self.driver.find_element_by_link_text("Public Profiles").click()
+		self.driver.implicitly_wait(10)
+		self.driver.find_element_by_link_text("My Profile").click()
+		self.driver.implicitly_wait(10)
+		self.driver.find_element_by_link_text("Public Posts").click()
+		self.driver.find_element_by_xpath('//input[@value="Log Out"]').click()
